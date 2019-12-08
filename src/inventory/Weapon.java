d37f0116619f3a89368999408e851b425c59c0f7
slownie <slownie16@gmail.com>
2019-12-07 22:02:29 -0500

    item and weapon setup

--- src/inventory/Weapon.java
+++ src/inventory/Weapon.java
@@ -1,4 +1,13 @@
 package inventory;
 
-public class Weapon {
+public class Weapon
+{
+    String name;
+    int stats[];
+
+    public Weapon(String name, int stats[])
+    {
+        this.name = name;
+        this.stats = stats;
+    }
 }
